87dad8f093ef18aad133817cb9fa08db7c22641e
Raven <rjsnavarette@gmail.com>
2025-01-18 11:25:17 +0800

    useState on job description

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
             <div className="md:ml-auto">
               <div className="flex space-x-2">
                 <a
-                  href="/index.html"
+                  href="/"
                   className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
                   >Home</a
                 >
